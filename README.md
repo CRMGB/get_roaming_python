0d04aa5736b87b8d769a2addf7bdc78735456753
Bisby91 <cristianbisby@gmail.com>
2020-11-22 20:12:05 +0000

    Refactor, created two functions for each type of table instead of having a function per each country

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 
 Simple Web Scraper to extract Monthly Roaming charges from the three.co.uk website,
-calling back to the UK, texting back to the UK, receiving a call and using internet data.
-From the following countries:
+calling back to the UK, texting back to the UK, receiving a call and using internet data,
+from the following countries:
     Brazil, South Africa, Portugal, Chile, Iceland, China, Madagascar.
 
-To start cd to selenium directory an type 'python3 search_roaming.py'
+It is set up for Firefox only.
+
+To start cd to selenium directory an type 'python3 search_roaming.py' or 'python search_roaming.py'
--- selenium/search_roaming.py
+++ selenium/search_roaming.py
@@ -1,127 +1,46 @@
 import lxml.html
 import re
+import enum
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-def start_selenium():
+websites_target_table_1 = {
+    "Brazil" : "http://www.three.co.uk/support/roaming/brazil",
+    "Portugal": "http://www.three.co.uk/support/roaming/portugal",
+    "Chile" : "http://www.three.co.uk/support/roaming/chile",
+    "Iceland" : "http://www.three.co.uk/support/roaming/iceland",
+}
+
+websites_target_table_2 = {
+    "South Africa": "http://www.three.co.uk/Support/Roaming_and_international/Mobile_roaming?content_aid=1214306363715",
+    "China" : "http://www.three.co.uk/Support/Roaming_and_international/Mobile_roaming?content_aid=1214306373940",
+    "Madagascar" : "http://www.three.co.uk/Support/Roaming_and_international/Mobile_roaming?content_aid=1214306362583",
+}
+
+def countries_table_1():
     driver = webdriver.Firefox()
+    table = './/table[@class="clearfix"]//tr'
+    for country, website in websites_target_table_1.items():
+        print(website, "\n")
+        driver.get(website)
+        root = lxml.html.fromstring(driver.page_source, table)
+        allowances = extract_table(root, table)
+        print(country, "--> ", allowances)
+    return driver
 
-    brazil_roaming(driver)
-    portugal_roaming(driver)
-    chile_roaming(driver)
-    china_roaming(driver)
-    s_africa_roaming(driver)
-    madagascar_roaming(driver)
-
+def countries_table_2(driver):
+    table = './/table[@class="roaming-charges-table"]//tr'
+    for country, website in websites_target_table_2.items():
+        print(website, "\n")
+        driver.get(website)
+        root = lxml.html.fromstring(driver.page_source, table)
+        allowances = extract_table(root, table)
+        print(country, "--> ", allowances)
     driver.close()
-
-def brazil_roaming(driver):
-
-    driver.get("http://www.three.co.uk/support/roaming/brazil")
-
-    if not re.search("Mobile roaming in Brazil - Support - Three", driver.title):
-        raise Exception("Unable to load google page!")
-    
-    root = lxml.html.fromstring(driver.page_source)
-
-    table = './/table[@class="clearfix"]//tr'
-
-    root = lxml.html.fromstring(driver.page_source, table)
-
-    brazil_allowances = extract_table(root, table)
-
-    print("brazil--> ", brazil_allowances)
-
-
-def portugal_roaming(driver):
-
-    driver.get("http://www.three.co.uk/support/roaming/portugal")
-
-    if not re.search("Mobile roaming in Portugal - Support - Three", driver.title):
-        raise Exception("Unable to load google page!")
-
-    table = './/table[@class="clearfix"]//tr'
-
-    root = lxml.html.fromstring(driver.page_source, table)
-
-    portugal_allowances = extract_table(root, table)
-
-    print("portugal--> ", portugal_allowances)
-
-
-def chile_roaming(driver):
-
-    driver.get("http://www.three.co.uk/support/roaming/chile")
-
-    if not re.search("Mobile roaming in Chile - Support - Three", driver.title):
-        raise Exception("Unable to load google page!")
-
-    table = './/table[@class="clearfix"]//tr'
-
-    root = lxml.html.fromstring(driver.page_source, table)
-
-    chile_allowances = extract_table(root, table)
-
-    print("chile--> ", chile_allowances)
-
-
-def s_africa_roaming(driver):
-
-    driver.get(
-        "http://www.three.co.uk/Support/Roaming_and_international/Mobile_roaming?content_aid=1214306363715"
-        )
-    if not re.search("Mobile roaming - Roaming & international - Support - Three", driver.title):
-        raise Exception("Unable to load google page!")
-
-    table ='.//table[@class="roaming-charges-table"]//tr'
-
-    root = lxml.html.fromstring(driver.page_source)
-
-    s_africa_allowances = extract_table(root, table)
-
-    print("s_africa--> ", s_africa_allowances)
-
-
-def china_roaming(driver):
-
-    driver.get(
-        "http://www.three.co.uk/Support/Roaming_and_international/Mobile_roaming?content_aid=1214306373940"
-        )
-
-    if not re.search("Mobile roaming - Roaming & international - Support - Three", driver.title):
-        raise Exception("Unable to load google page!")
-
-    table ='.//table[@class="roaming-charges-table"]//tr'
-
-    root = lxml.html.fromstring(driver.page_source)
-
-    china_allowances = extract_table(root, table)
-
-    print("china_allowances--> ", china_allowances)
-
-def madagascar_roaming(driver):
-
-    driver.get(
-        "http://www.three.co.uk/Support/Roaming_and_international/Mobile_roaming?content_aid=1214306362583"
-        )
-    if not re.search("Mobile roaming - Roaming & international - Support - Three", driver.title):
-        raise Exception("Unable to load google page!")
-
-    table ='.//table[@class="roaming-charges-table"]//tr'
-
-    root = lxml.html.fromstring(driver.page_source)
-
-    madagascar_allowances = extract_table(root, table)
-
-    print("madagascar_allowances--> ", madagascar_allowances)
-
 
 def extract_table(root, table):
     table_allowances = {}
-    uk_search = 'UK'
-    re_call_search = 'calls from any number'
-    re_internet = 'Using internet and data'
 
     for row in root.xpath(table):
         headers = row.xpath('.//th/text()')
@@ -129,10 +48,10 @@ def extract_table(root, table):
         #Search for the right allowances
         filter_header = [
             header for header in headers 
-            if re.search(uk_search, header) 
-            or re.search(re_call_search, header)
-            or re.search(re_internet, header)
-            ] 
+            if re.search('UK', header) 
+            or re.search('calls from any number', header)
+            or re.search('Using internet and data', header)
+        ] 
 
         if len(filter_header) !=0 and len(cells)>1:
             if re.search("roaming-charges-table", table):
@@ -142,4 +61,5 @@ def extract_table(root, table):
     return table_allowances
 
 if __name__ == '__main__':
-    start_selenium()
+    driver = countries_table_1()
+    countries_table_2(driver)
