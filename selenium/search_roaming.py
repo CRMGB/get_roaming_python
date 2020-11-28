4d8624e7e1801c1120ff743f01c6a8c946be6cd2
Bisby91 <cristianbisby@gmail.com>
2020-11-28 12:39:37 +0000

    Reformating search_roaming.py: - reformat to include the two different tables in a nested dicctionary. - Formatted methods to simplify and read the new dicctionary properly

--- selenium/search_roaming.py
+++ selenium/search_roaming.py
@@ -1,43 +1,37 @@
 import lxml.html
 import re
-import enum
+import json
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-websites_target_table_1 = {
-    "Brazil" : "http://www.three.co.uk/support/roaming/brazil",
-    "Portugal": "http://www.three.co.uk/support/roaming/portugal",
-    "Chile" : "http://www.three.co.uk/support/roaming/chile",
-    "Iceland" : "http://www.three.co.uk/support/roaming/iceland",
+tables = {
+    './/table[@class="clearfix"]//tr': {
+        "Brazil" : "http://www.three.co.uk/support/roaming/brazil",
+        "Portugal": "http://www.three.co.uk/support/roaming/portugal",
+        "Chile" : "http://www.three.co.uk/support/roaming/chile",
+        "Iceland" : "http://www.three.co.uk/support/roaming/iceland",
+    }, 
+    './/table[@class="roaming-charges-table"]//tr' : {
+        "South Africa": "http://www.three.co.uk/Support/Roaming_and_international/Mobile_roaming?content_aid=1214306363715",
+        "China" : "http://www.three.co.uk/Support/Roaming_and_international/Mobile_roaming?content_aid=1214306373940",
+        "Madagascar" : "http://www.three.co.uk/Support/Roaming_and_international/Mobile_roaming?content_aid=1214306362583",
+    }
 }
 
-websites_target_table_2 = {
-    "South Africa": "http://www.three.co.uk/Support/Roaming_and_international/Mobile_roaming?content_aid=1214306363715",
-    "China" : "http://www.three.co.uk/Support/Roaming_and_international/Mobile_roaming?content_aid=1214306373940",
-    "Madagascar" : "http://www.three.co.uk/Support/Roaming_and_international/Mobile_roaming?content_aid=1214306362583",
-}
-
-def countries_table_1():
+def initialize_selenium():
     driver = webdriver.Firefox()
-    table = './/table[@class="clearfix"]//tr'
-    for country, website in websites_target_table_1.items():
-        print(website, "\n")
-        driver.get(website)
-        root = lxml.html.fromstring(driver.page_source, table)
-        allowances = extract_table(root, table)
-        print(country, "--> ", allowances)
-    return driver
-
-def countries_table_2(driver):
-    table = './/table[@class="roaming-charges-table"]//tr'
-    for country, website in websites_target_table_2.items():
-        print(website, "\n")
-        driver.get(website)
-        root = lxml.html.fromstring(driver.page_source, table)
-        allowances = extract_table(root, table)
-        print(country, "--> ", allowances)
+    for table, targets in tables.items():
+        loop_countries_tables(driver, table, targets)
     driver.close()
+
+def loop_countries_tables(driver, table, targets):
+    for country, website in targets.items():
+        print(website, "\n")
+        driver.get(website)
+        root = lxml.html.fromstring(driver.page_source, table)
+        allowances = extract_table(root, table)
+        print(country, "--> ", allowances)
 
 def extract_table(root, table):
     table_allowances = {}
@@ -61,5 +55,4 @@ def extract_table(root, table):
     return table_allowances
 
 if __name__ == '__main__':
-    driver = countries_table_1()
-    countries_table_2(driver)
+    initialize_selenium()
